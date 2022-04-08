3d42815ee0ca94368c4cfab1e258d4247a17b0f7
bradleyjohnsxn <62025141+bradleyjohnsxn@users.noreply.github.com>
2022-04-08 16:40:18 -0400

    Added function to load ics file.

--- FileIO.cpp
+++ FileIO.cpp
@@ -3,9 +3,12 @@
 #include <iostream>
 #include "task.h"
 #include <string>
+#include "FileIO.h"
 
 using namespace std;
 
+
+/*--------------------------------------- File I/O ----------------------------------------*/
 vector<task> Open_File() {
 	ifstream tasks("Resources/tasks.tsk");
 	vector<task> returner;
@@ -68,4 +71,126 @@ void Write_File(vector<task> tasks){
 			save_file << "END\n";
 		}
 	}
+}
+
+
+/*--------------------------------------- Load ICS ----------------------------------------*/
+vector<task> load_ics(string file_path, vector<task>& existing) {
+
+    string holder, name, course;
+    vector<task> added;
+
+    ifstream in_file;
+    in_file.open(file_path);
+
+    while (1) {
+        getline(in_file, holder);
+        if (holder.substr(0,13) == "X-WR-CALDESC:") break;
+    }
+
+    bool leave = false;
+    while (getline(in_file, holder)) {
+        if (holder == "BEGIN:VEVENT"){
+            leave = false;
+            task hodl;
+
+            while (getline(in_file, holder)) {
+                if (holder.substr(0, 5) == "DTEND")
+                    hodl.set_duedate(decode_date(only_numbers(holder)));
+
+                if (holder.substr(0, 12) == "DESCRIPTION:") {
+                    if (holder.find("Zoom") != string::npos) {
+                        leave = true;
+                        break;
+                    }
+                    else {
+                        string temp = holder.substr(12);
+
+                        string s = "";
+                        getline(in_file, s);
+                        while (s.substr(0, 9) != "SEQUENCE:" && s.substr(0, 9) != "LOCATION:") {
+                            temp += s;
+                            getline(in_file, s);
+                        }
+                        hodl.set_description(temp);
+                    }
+                }
+
+                if (holder.substr(0, 8) == "SUMMARY:") {
+                    decode_summary(holder.substr(8), name, course);
+                    hodl.set_course(course);
+                    hodl.set_name(name);
+                    break;
+                }
+            }
+            if (leave == false) added.push_back(hodl);
+        }
+    }
+
+    in_file.close();
+
+    for (unsigned int i = 0; i < added.size(); i++) {
+        existing.push_back(added[i]);
+    }
+    return existing;
+}
+
+/*--------------------------------------- Helpers ----------------------------------------*/
+
+string decode_date(string in) {
+    string hold = in, m, d, y;
+    y = hold.substr(0, 4);
+    m = hold.substr(4, 2);
+    d = hold.substr(6, 2);
+    return m + "-" + d + "-" + y;
+}
+
+void decode_summary(string sum, string& name, string& course) {
+    string n, c;
+    bool add = true, cl = false;
+    for (unsigned int i = 0; i < sum.length(); i++) {
+        if (sum[i] == '(')
+            add = false;
+        else if (sum[i] == ')')
+            add = true;
+        else if (sum[i] == '[') {
+            cl = true;
+            add = false;
+        }
+        else if (sum[i] == ']') {
+            cl = false;
+            add = false;
+        }
+
+        if (add == true && sum[i] != ')') {
+            n += sum[i];
+        }
+        else if (cl == true && sum[i] != '[') {
+            c += sum[i];
+        }
+    }
+    n = trim(n);
+    name = n;
+    course = c;
+}
+
+string only_numbers(string s) {
+    string t = "";
+    for (char x : s) {
+        if (isdigit(x)) {
+            t += x;
+        }
+    }
+    return t;
+}
+
+string trim(string s) {
+    while(true) {
+        if (s[s.length() - 1] == ' ') {
+            s = s.substr(0, s.length() - 1);
+        }
+        else
+            break;
+    }
+    return s;
 }
--- FileIO.h
+++ FileIO.h
@@ -1,5 +1,16 @@
 #pragma once
 #include <vector>
 #include "task.h"
+
+/*------------------------ File I/O ------------------------*/
 vector<task> Open_File();
 void Write_File(vector<task> tasks);
+
+/*------------------------ Load ICS ------------------------*/
+vector<task> load_ics(string file_path, vector<task>& existing);
+
+/*------------------------ Helpers -------------------------*/
+string decode_date(string in);
+void decode_summary(string sum, string& name, string& course);
+string only_numbers(string s);
+string trim(string s);
