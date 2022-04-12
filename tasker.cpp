ea138a051b9f5d1a247cdeaf139fb7dc3be950d1
ThePurplePigeon <joshua@hughesonthenet.com>
2022-04-11 20:12:23 -0400

    Base Layout
    Added layout, plus function shells for pressing buttons and toggling the textbox

--- tasker.cpp
+++ tasker.cpp
@@ -13,3 +13,27 @@ Tasker::~Tasker()
     delete ui;
 }
 
+
+void Tasker::on_checkBox_stateChanged(int arg1)
+{
+    //Checkbox for marking a task as complete
+}
+
+
+void Tasker::on_pushButton_clicked()
+{
+    //Delete Task
+}
+
+
+void Tasker::on_pushButton_2_clicked()
+{
+    //Submit Changes to task
+}
+
+
+void Tasker::on_pushButton_5_clicked()
+{
+    //Add new task
+}
+
